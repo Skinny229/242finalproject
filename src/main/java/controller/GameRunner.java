bc95271d903bb557581501eac4e4501a7fefe3fd
vdesai575 <64618725+vdesai575@users.noreply.github.com>
2020-05-06 15:53:31 -0400

    Update GameRunner.java

--- src/main/java/controller/GameRunner.java
+++ src/main/java/controller/GameRunner.java
@@ -14,7 +14,6 @@ public class GameRunner {
     private static Player player1;
     private static Player player2;
 
-
     static int onPlayer = 1;
 
     public GameRunner()  {
@@ -25,7 +24,8 @@ public class GameRunner {
         new ShipPlacer(player2);
 
 
-        //Wait for ship placement to be done
+        // Waits for ship placement to be done
+        
         while(!player1.allShipsPlaced() || !player2.allShipsPlaced()) {
             try {
                 Thread.sleep(200);
@@ -34,19 +34,14 @@ public class GameRunner {
             }
         }
 
-
-       //Player vs AI
+       // Player vs Player
+       // Player vs AI
+        
         new PlayerFrame(player1);
         new AIFrame(player2);
-
-
-        //Player vs Player
-
     }
 
-
     public synchronized static String AIFire() {
-
 
         int y ;
         int x ;
@@ -60,27 +55,24 @@ public class GameRunner {
         return (String) (y+""+x);
     }
 
-
     public synchronized PlayerView getPlayer1(){
         return new PlayerView(player1, player2);
     }
-
 
     public synchronized PlayerView getPlayer2(){
         return new PlayerView(player1, player2);
     }
 
-
     public synchronized static FireResponse firePlayer1(int y, int x){
         return player2.beingFiredOn(y,x);
     }
-
 
     public synchronized static FireResponse firePlayer2(int y, int x){
         return player1.beingFiredOn(y,x);
     }
 
-
+    // Determines a winner when all of ones side ships are sunk
+    
     public int hasWinner(){
         if(!player2.isAllShipsDown() && !player1.isAllShipsDown())
             return 0;
@@ -89,10 +81,4 @@ public class GameRunner {
         else
             return 2;
     }
-
-
-
-
-
-
 }
